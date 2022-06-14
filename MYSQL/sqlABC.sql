b40b2b2ab7526238796b29d2b1aa6fff06468afb
Daniel Diaz Ovalle <drdiaz04@misena.edu.com>
2022-06-14 00:43:50 -0500

    version2.0

--- MYSQL/sqlABC.sql
+++ MYSQL/sqlABC.sql
@@ -4,8 +4,8 @@ USE empresaABC;
 create table proveedor (
 documento_pro integer not null, 
 apellido_pro varchar(20), 
-direccion_pro varchar(255), 
-email_pro varchar(255), 
+direccion_pro varchar(50), 
+email_pro varchar(50), 
 name_pro varchar(20), 
 vehiculos_pro integer, 
 primary key (documento_pro)) engine=InnoDB
